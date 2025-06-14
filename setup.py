3d21e9af28efbea4edfaf017f4bdfaa36e9a0ea7
Fathima Farwin <farwiinm@gmail.com>
2025-06-14 16:44:23 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+#can be used to build our application into a package
+
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+  '''
+  this function will return a list of requirements
+  '''
+  requirements=[]
+  with open(file_path) as file_obj:
+    requirements=file_obj.readlines()
+    requirements=[req.replace("\n","") for req in requirements]
+
+  if '-e .' in requirements:
+     requirements.remove('-e .')
+
+  return requirements
+
+setup(
+  name='student-pred',
+  version='0.0.1',
+  author='Fathima-Farwin',
+  author_email='farwiinm@gmail.com',
+  packages=find_packages(),
+  install_requires=get_requirements('requirements.txt')
+)
